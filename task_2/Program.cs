c6396e470114e3df3041f310338bc9d079268360
Orley <mbe.orley@yandex.ru>
2022-11-01 20:07:58 +0300

    commit 1

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// 2.Напишите программу, которая принимает на
+// вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите число 1");
+string value1 = Console.ReadLine();
+int a = Convert.ToInt32(value1);
+Console.WriteLine("Введите число 2");
+string value2 = Console.ReadLine();
+int b = Convert.ToInt32(value2);
+Console.WriteLine("Введите число 3");
+string value3 = Console.ReadLine();
+int c = Convert.ToInt32(value3);
+if (a > b && a > c )
+{
+    Console.WriteLine($"Число {a} максимальное число");
+}
+else if (b > a && b > c )
+{
+   Console.WriteLine($"Число {b} максимальное число");
+}
+else if (c > a && c > b)
+{
+   Console.WriteLine($"Число {c} максимальное число");
+}

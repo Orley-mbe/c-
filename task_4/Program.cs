fbeded72e7fe84741b74198d9cf52902829deef2
Orley <mbe.orley@yandex.ru>
2022-11-02 18:53:44 +0300

    commit 4

--- task_4/Program.cs
+++ task_4/Program.cs
@@ -1,4 +1,13 @@
 ﻿// Напишите программу, которая на вход
 //принимает число (N), а на выходе показывает все чётные
 //числа от 1 до N.
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число");
+string value = Console.ReadLine();
+int a = Convert.ToInt32(value);
+for (int counter = 0;  counter <= a; counter++  )
+{   if (counter % 2 == 0)
+    {
+      Console.WriteLine($"{counter}");
+    }
+}
+

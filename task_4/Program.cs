1d5b703c886cd8147fb3b0934c71498fc46b65e7
Orley <mbe.orley@yandex.ru>
2022-11-02 18:54:33 +0300

    commit 3

--- task_4/Program.cs
+++ task_4/Program.cs
@@ -4,10 +4,11 @@
 Console.WriteLine("Введите число");
 string value = Console.ReadLine();
 int a = Convert.ToInt32(value);
-for (int counter = 0;  counter <= a; counter++  )
-{   if (counter % 2 == 0)
+for (int counter = 0; counter <= a; counter++)
+{
+    if (counter % 2 == 0)
     {
-      Console.WriteLine($"{counter}");
+        Console.WriteLine($"{counter}");
     }
 }
 

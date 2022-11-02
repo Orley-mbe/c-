c5c99de493e4ec69beb354f5ea13a752adef79a6
Orley <mbe.orley@yandex.ru>
2022-11-02 18:32:31 +0300

    commit 1

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход
+//принимает число и выдаёт, является ли число чётным
+//(делится ли оно на два без остатка).
+Console.WriteLine("Введите число");
+string value = Console.ReadLine();
+int a = Convert.ToInt32(value);
+if (a%2==0)
+{
+    Console.WriteLine($"Число {a} четное");
+}
+else
+{
+     Console.WriteLine($"Число {a} нечетное");
+}
+

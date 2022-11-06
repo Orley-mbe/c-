61c3968a0da605d5bea5eedbc3684c0c5a03d1a9
Orley <mbe.orley@yandex.ru>
2022-11-06 15:08:53 +0300

    commit 1

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // Задача 2: Напишите программу, которая выводит
+// случайное трёхзначное число и удаляет вторую цифру
+// этого числа. Не использовать строки для расчета.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int number = Prompt("Введите число  ");
+if (number > 100 && number < 1000)
+{
+    int temp = (number / 100) * 10 + (number % 10);
+    Console.WriteLine(temp);
+}
+else
+{
+    Console.WriteLine("Попробуйте еще раз");
+}

a72d1bc36c540acd66c269fb19e2d0489381c97d
Orley <mbe.orley@yandex.ru>
2022-11-06 14:46:37 +0300

    commit 1

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -3,11 +3,23 @@
 // цифру этого числа. Не использовать строки для расчета.
 
 
-int Prompt(string message);
+int Prompt(string message)
 {
     System.Console.Write(message);
-    int number = Convert.ToInt32(Console.Readline());
+    int number = Convert.ToInt32(Console.ReadLine());
     return number; 
 }
+
+int number = Prompt("Введите число  ");
+if(number < 1000 && number >100)
+{
+    int temp = (number %100)/10;
+    Console.Write($"Последняя цифра {temp}");
+}
+else
+{
+    Console.Write($"Попробуйте еще раз");
+}
+
 
 

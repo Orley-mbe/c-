6a28dc02b7cc595d405c7d3ab4ddc7130d0018a3
Orley <mbe.orley@yandex.ru>
2022-11-06 14:13:22 +0300

    Create task_1

---
+++ task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 1: Напишите программу, которая принимает на
+//вход трёхзначное число и на выходе показывает вторую
+// цифру этого числа. Не использовать строки для расчета.
+
+
+int Prompt(string message);
+{
+    System.Console.Write(message);
+    int number = Convert.ToInt32(Console.Readline());
+    return number; 
+}
+
+

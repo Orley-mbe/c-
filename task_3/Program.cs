657d6e2a7c1e0f866391523d2ef0030c45efd93a
Orley <mbe.orley@yandex.ru>
2022-11-06 16:06:44 +0300

    commit 1

---
+++ task_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// // Задача 3: Напишите программу, которая выводит третью
+// цифру заданного числа или сообщает, что третьей цифры
+// нет. Не использовать строки для расчета.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int number = Prompt("Введите число  ");
+if (number / 100 > 1)
+{
+    int temp = number % 10;
+    Console.WriteLine($"{temp}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}

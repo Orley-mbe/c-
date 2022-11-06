8dc8af0197048f5b00d3ab3f74134749add6adc1
Orley <mbe.orley@yandex.ru>
2022-11-06 19:08:04 +0300

    commit 1

--- task_4/Program.cs
+++ task_4/Program.cs
@@ -3,5 +3,34 @@
 // является ли этот день выходным.
 
 
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32( Console.ReadLine());
+    return number;
+}
 
 
+bool Dayoff(int number)
+{
+  if(number > 8)
+  {
+    Console.WriteLine($"Это число не подходит ");
+    return false;
+  }
+    return true; 
+}
+
+int number = Prompt("Введите число обозначающее день недели ");
+
+if(Dayoff(number))
+{
+    if(number == 6 || number == 7)
+    {
+         Console.WriteLine($"Сегодня {number} день недели - выходной");
+    }
+    else
+    {
+      Console.WriteLine("Это не выходной день ");  
+    }
+}

b42f797db3e88ea2ee5528f5843b04fae05b25ea
Orley <mbe.orley@yandex.ru>
2022-11-12 00:05:08 +0300

    commit 1

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -4,6 +4,16 @@
 int Prompt(string message)
 {
     System.Console.Write(message);
-    return int.Parse.Console.ReadLine();
+    return int.Parse(Console.ReadLine());
+}
+
+//int []GetArr(){
+
+int N = Prompt("Введите число N: ");
+
+for(int i=1; i<=N; i++)
+{
+    int a = i*i*i;
+    Console.WriteLine($"{i}^3={a}");
 }
 

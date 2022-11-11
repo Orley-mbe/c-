f7b5971217e31d6d20a79579943c6815060e4f11
Orley <mbe.orley@yandex.ru>
2022-11-11 23:23:09 +0300

    create task_3

---
+++ task_3/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse.Console.ReadLine();
+}
+

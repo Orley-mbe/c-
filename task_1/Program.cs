e2047a4c63c5a8f088b28998c1327f5f8fcf44b1
Orley <mbe.orley@yandex.ru>
2022-11-11 21:33:20 +0300

    commit 1

---
+++ task_1/Program.cs
@@ -0,0 +1,44 @@
+﻿//  Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+
+int Prompt(string message)
+{
+    System.Console.Write(message); // выводит сообщение в консоль
+    return int.Parse(Console.ReadLine()); // считывает текст с консоли и преобразует в число
+}
+
+bool Validate(int number)
+{
+    if (number < 10000)
+    {
+        Console.WriteLine("Введите другое число, это не подходит");
+        return false;
+    }
+    return true;
+}
+
+int number = Prompt("Введите пятизначное число ");
+if (Validate(number))
+{
+    int a = number / 10000;
+    if(a == number % 10)
+    {
+        
+        if ((number%10000)/1000 == (number/10)%10)
+        {
+            Console.Write($"Число {number} является палидромом");
+        }
+        else
+        {
+            Console.Write($"Число {number} не является палидромом");
+        }
+    }
+    else
+    {
+        Console.Write($"Число {number} не является палидромом");
+    }
+}
+
+
+
+

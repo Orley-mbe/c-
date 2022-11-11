7cbaa110103b9c9c0c6b8eac7c08bd9304a755c9
Orley <mbe.orley@yandex.ru>
2022-11-11 23:20:59 +0300

    commit 1

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,4 +1,22 @@
 ﻿// Задача 2: Напишите программу, которая принимает на вход координаты двух точек 
 //и находит расстояние между ними в 3D пространстве.
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
 
-Console.WriteLine("Hello, World!");
+int x1 = Prompt("Введите координату X1: ");
+int x2 = Prompt("Введите координату X2: ");
+int y1 = Prompt("Введите координату Y1: ");
+int y2 = Prompt("Введите координату Y2: ");
+int z1 = new Random().Next(1, 100);
+int z2 = new Random().Next(1, 100);
+
+int x = x2 - x1;
+int y = y2 - y1;
+int z = z2 - z1;
+
+double Lenght = Math.Sqrt(x*x + y*y +z*z);
+Console.WriteLine(Lenght);
+

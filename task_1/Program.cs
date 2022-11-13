186bd6697fc51e725b8e56cbd3bd06d4e4c6ce68
Orley <mbe.orley@yandex.ru>
2022-11-13 17:26:08 +0300

    commit 1

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -2,4 +2,20 @@
 //два числа (A и B) и возводит число A в натуральную
 //степень B. Реализовать через функции.
 
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int DegreeOf(int a, int b)
+{
+    int i = (int)Math.Pow(a, b);
+    return i;
+}
+
+int a = Prompt("введите число 1: ");
+int b = Prompt("введите число 2: ");
+Console.WriteLine($"{a}^{b}={DegreeOf(a, b)}");
+
 

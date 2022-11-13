5a35f5029471f85dde664bdb743ea61d468530f7
Orley <mbe.orley@yandex.ru>
2022-11-13 19:52:31 +0300

    commit 1

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -4,4 +4,30 @@
 //(пригодится в следующих задачах). Реализовать через
 //функции.
 
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
 
+int[] GetArr(int a)
+{
+    int[] tempArr = new int[a];
+    for(int i =0; i <a;i++)
+    {
+        tempArr[i] = new Random().Next(1, 1000);
+    }
+    return tempArr;
+}
+
+void printArr(int[] intArr)
+{
+    Console.Write(value:$"{intArr[0]}");
+    for(int i = 1; i < intArr.Length; i++)
+    {
+        Console.Write(value:$", {intArr[i]}");
+    }
+}
+int a = Prompt("Введите длину массива: ");
+Console.Write(value: $"случайный массив длиной {a}");
+printArr(GetArr(a));

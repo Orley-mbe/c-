c14ebc0b0b5a9fc7295feede0c03520cd3b03132
Orley <mbe.orley@yandex.ru>
2022-11-13 18:41:42 +0300

    commit 1

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -2,5 +2,25 @@
 //вход число и выдаёт сумму цифр в числе. Реализовать
 //через функции.
 
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int GetNum(int a)
+{  
+    int sum=0;
+    while(a > 1)
+    {
+        sum=sum + a%10;
+        a= a/10;       
+    }
+    return sum;
+}
+
+int a = Prompt("Введите число: ");
+int sum = GetNum(a);
+Console.WriteLine($"Сумма чисел числа равна {sum}");
 
 

da538f530ed6659f9eb4edd4ee9db9dad2de8fd1
Orley <mbe.orley@yandex.ru>
2022-11-27 12:38:12 +0300

    commit 1

---
+++ task_1/Program.cs
@@ -0,0 +1,57 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int Prompt(string msg)
+{
+    Console.WriteLine(msg);
+    int len = Convert.ToInt32(Console.ReadLine());
+    return len;
+}
+
+int[] CreateArr(int len, int minNum, int maxNum)
+{
+    int[] arr = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = Prompt("введите число ");
+    }
+    return arr;
+}
+
+int Count(int[] arr)
+{
+    int temp = 0;
+    foreach (var item in arr)
+    {
+        if (item > 0) 
+        {
+            temp++;
+        }        
+    }
+    return temp;
+}
+
+
+
+
+void PrintArr(int[] arr)
+{
+    foreach (var item in arr)
+    {
+        System.Console.Write($"{item}");
+    }
+}
+
+void Runner()
+{
+    int len = Prompt("Введите размер массива");
+    int minNum = Prompt("Введите минимальное число");
+    int maxNum = Prompt("Введите максимальное число");
+    int[] numbers = CreateArr(len, minNum, maxNum);
+    int result = Count(numbers);
+    PrintArr(numbers);
+    System.Console.WriteLine();
+    System.Console.WriteLine($"Количество чисел больше 0 = {result}");
+}
+
+Runner();

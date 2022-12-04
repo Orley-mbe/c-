f51f6a86aa2e4cb8c818a8c294ca587d7375571f
Orley <mbe.orley@yandex.ru>
2022-12-04 21:57:07 +0300

    commit 2

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -9,7 +9,7 @@ void CreateArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; i < array.GetLength(1); j++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
             array[i, j] = new Random().Next(0, 10);
         }
